67dffe0bc2e0aa16bd150effbbd7712d362a0388
Diogo Nunes <diogo_p_nunes@hotmail.com>
2018-11-20 09:02:58 +0100

    Added random array visualization

--- sketch.js
+++ sketch.js
@@ -1,11 +1,32 @@
+var width;
+var height;
+
+var arr = [];
+var arrSize;
+var zoom = 10;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  background(50)
+  width = windowWidth;
+  height = windowHeight;
+  createCanvas(width, height);
+  background(50);
 
+  initArray();
+}
+
+function initArray() {
+  arrSize = floor(width/zoom);
+  for(var i=0; i < arrSize; i++) {
+    arr.push(floor(random(height)));
+  }
 }
 
 
 function draw() {
-
+  scale(10, 1);
+  background(50);
+  stroke(255, 255, 255);
+  for(var i=0; i < arrSize; i++) {
+    line(i, 0, i, arr[i]);
+  }
 }
